63c330a0a467525bf989d88624f7f03a3194fd25
Viral Patel <viral.patel@fourth.com>
2018-08-30 19:08:17 +0100

    adding ring.py

--- ring.py
+++ ring.py
@@ -1,12 +1,12 @@
 from ring_doorbell import Ring
-myring = Ring('','')
+myring = Ring('', '')
 print(myring.devices)
 
 
 for doorbell in myring.doorbells:
 
     # listing the last 15 events of any kind
-    for event in doorbell.history(limit=1):
+    for event in doorbell.history(limit = 1):
         print('ID:       %s' % event['id'])
         print('Kind:     %s' % event['kind'])
         print('Answered: %s' % event['answered'])
@@ -14,4 +14,4 @@ for doorbell in myring.doorbells:
         print('--' * 50)
 
     # get a event list only the triggered by motion
-    events = doorbell.history(kind='motion')
+    events = doorbell.history(kind = 'motion')
