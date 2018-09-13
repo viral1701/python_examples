63a087910fb831e7f0795437b751c9b17c8198b6
Viral Patel <viral.patel@fourth.com>
2018-09-13 09:04:09 +0100

    working on examples

--- game_example/alien_invasion.py
+++ game_example/alien_invasion.py
@@ -2,21 +2,21 @@ import sys
 
 import pygame
 
+from settings import Settings
+from ship import Ship
+
 def run_game():
         pygame.init()
-        screen = pygame.display.set_mode((1200,800))
+        ai_settings = Settings()
+        screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
-        #Set the background colour.
-        bg_colour = (230, 230, 230)
+        ship = Ship(screen)
+        
 
-        # Start the main loop for the game.
         while True:
-            #Watch for keyboard and mouse events
-            screen.fill(bg_colour)
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            screen.fill(ai_settings.bg_colour)
+            ship.blitme()
 
             pygame.display.flip()
 
---
+++ game_example/settings.py
@@ -0,0 +1,8 @@
+class Settings():
+    """ A class to store all settings for the game example """
+
+    def __init__(self):
+        #Game Settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_colour = (230, 230, 230)
